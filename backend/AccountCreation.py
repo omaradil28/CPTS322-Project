4b3471e252452216c7f1479422785089a9489c73
omaradil28 <omar.adil9492@gmail.com>
2025-03-24 17:17:00 -0700

    .

--- backend/AccountCreation.py
+++ backend/AccountCreation.py
@@ -1,7 +1,7 @@
 import json
 import hashlib
 import os
-from backend.Verification import verification
+from Verification import verification
 
 USER_FILE = "users.json"
 DOMAINS_FILE = "emails.json"
