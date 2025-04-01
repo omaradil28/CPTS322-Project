bf3a276609c3631118eba91f6df39272bdeac189
omaradil28 <omar.adil9492@gmail.com>
2025-04-01 04:18:08 -0700

    Login

---
+++ backend/Login.py
@@ -0,0 +1,47 @@
+import hashlib
+import json
+import os
+
+USER_FILE = "users.json"
+
+def load():
+    if os.path.exists(USER_FILE):
+        try:
+            with open(USER_FILE, "r") as file:
+                data = file.read().strip()
+                return json.loads(data) if data else {}
+        except json.JSONDecodeError:
+            return {}
+    return {}
+
+def hashing(password):
+    return hashlib.sha256(password.encode()).hexdigest()
+
+def login():
+    os.system('clear')
+    print("Login\n")
+    users = load()
+
+    while True:
+        username = input("Enter your username: ").strip()
+        if username in users:
+            break
+        else:
+            print("Username not found. Try again.")
+            print()
+
+    while True:
+        password = input("Enter your password: ").strip()
+        if hashing(password) == users[username]["password"]:
+            break
+        else:
+            print("Incorrect password. Try again.")
+            print()
+
+    print()
+    print(f"Login successful! Welcome back, {username}.")
+
+    profession = users[username]["profession"]
+    print(f"You are logged in as a {profession}.")
+
+login()
